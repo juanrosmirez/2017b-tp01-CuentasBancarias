478c9c19e677b097792d65854e1222c2fe46d18f
Juan Ignacio Rosmirez <juanrosmirez@gmail.com>
2017-09-08 23:51:52 -0300

    Azul
    Se mejora el metodo de transferencia para que verifique si las cuentas se encuentran habilitadas para realizar transferencias

--- CuentasBancarias/src/cuentasBancarias/CuentasBancarias.java
+++ CuentasBancarias/src/cuentasBancarias/CuentasBancarias.java
@@ -3,13 +3,20 @@ package cuentasBancarias;
 public class CuentasBancarias {
 	
 	private double saldo;
+	private boolean estado;
+	
+	public CuentasBancarias(){
+		this.saldo=0;
+		this.estado=true;
+	}
 	
 	public CuentasBancarias(double saldo) {
 		this.saldo=saldo;
+		this.estado=true;
 	}
 
 	public int transferirMontoHacia(double monto, CuentasBancarias cuentaDestino) {
-		if(monto<=saldo){
+		if(monto<=saldo && estado==true && cuentaDestino.estado==true){
 			cuentaDestino.saldo+=monto;
 			this.saldo-=saldo;
 			return 1;
