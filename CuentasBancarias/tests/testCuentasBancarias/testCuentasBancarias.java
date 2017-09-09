59ed626f723ecb9ee6a37a3966098c3d3767cdc7
Juan Ignacio Rosmirez <juanrosmirez@gmail.com>
2017-09-09 00:37:39 -0300

    Rojo
    Una cuenta de banco no puede tener saldo<0 y transfiere igual

--- CuentasBancarias/tests/testCuentasBancarias/testCuentasBancarias.java
+++ CuentasBancarias/tests/testCuentasBancarias/testCuentasBancarias.java
@@ -11,14 +11,19 @@ public class testCuentasBancarias {
 	@Test
 	public void haySaldoParaTransferencia() {
 		
-		CuentasBancarias cuentaOrigen = new CuentasBancarias(20);
+		CuentasBancarias cuentaOrigen = new CuentasBancarias(-50);
 		CuentasBancarias cuentaDestino = new CuentasBancarias(0);
 		
 		assertEquals(1,cuentaOrigen.transferirMontoHacia(15,cuentaDestino)); 
 	}
+	
 	@Test
 	public void transfiereAUnaMismaCuenta(){
+		
 		CuentasBancarias cuentaOrigen = new CuentasBancarias(20);
 		assertEquals(0,cuentaOrigen.transferirMontoHacia(15,cuentaOrigen));
 	}
+	
+	
+	
 }
